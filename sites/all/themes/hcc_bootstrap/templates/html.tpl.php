4bdcb8027898669bf33f956e5a07e307f042b13a
hccadmin <dave_rothfarb@dfci.harvard.edu>
2015-10-20 12:41:40 -0400

    Added Google Analytics, xml sitemap, modified htaccess to rewrite belferinstitute url to belfercenter

--- sites/all/themes/hcc_bootstrap/templates/html.tpl.php
+++ sites/all/themes/hcc_bootstrap/templates/html.tpl.php
@@ -61,6 +61,18 @@
   <![endif]-->
   <script src="https://use.typekit.net/jci8jtl.js"></script>
   <script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69061193-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
   <!--[if lt IE 9]>
     <body class="<?php print $classes . ' ieclass'; ?>" <?php print $attributes;?>>
